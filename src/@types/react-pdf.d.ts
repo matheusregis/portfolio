22c9fb265bfe42014951e99dd5889e8ed7649365
Matheus Regis <matheustrai@gmail.com>
2024-11-24 22:44:35 -0500

    fix - errors lib

--- src/@types/react-pdf.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare module "react-pdf" {
-    export const pdfjs: any;
-    export const Document: any;
-    export const Page: any;
-  }
-  
--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -1,39 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Button, Row } from "antd";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import pdf from "../../assets/resume.pdf";
+import imageResume from "../../assets/resume.jpg";
 import * as Styled from "./styles";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
 const ResumeNew: React.FC = () => {
-  const [width, setWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <Styled.Container>
       <Row justify="center" style={{ marginBottom: "20px" }}>
-        <Button type="primary" href={pdf} target="_blank" style={{ maxWidth: "250px" }}>
+        <Button type="primary" href={pdf} target="_blank" style={{ maxWidth: "300px" }}>
           <AiOutlineDownload />
           &nbsp;Download CV
         </Button>
       </Row>
 
       <Row justify="center" className="resume">
-        <Document
-          file={pdf}
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          onLoadError={(error: any) => console.error("Error loading PDF:", error)}
-        >
-          <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} canvasBackground="black"  />
-        </Document>
+        <img src={imageResume} alt="Resume" style={{ maxWidth: "66%", height: "auto" }} />
       </Row>
 
       <Row justify="center" style={{ marginTop: "20px" }}>
